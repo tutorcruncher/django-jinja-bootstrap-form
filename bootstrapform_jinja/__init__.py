049c0fbad287091a0a6842e5ccafe7c3be22a92a
Tom Hamilton Stubber <tomhamiltonstubber@gmail.com>
2022-10-25 16:19:39 +0100

    Uprev to 3.10 and fix the version

--- bootstrapform_jinja/__init__.py
+++
@@ -1,9 +0,0 @@
-from .meta import VERSION
-import django
-
-__version__ = str(VERSION)
-if django.VERSION >= (3, 2):
-    # The declaration is only needed for older Django versions
-    pass
-else:
-    default_app_config = 'bootstrapform_jinja.apps.BootstrapformJinjaConfig'
--- bootstrapform_jinja/meta.py
+++
@@ -1,4 +0,0 @@
-from distutils.version import StrictVersion
-
-# start from a fresh number 4 to identify jinja verison
-VERSION = StrictVersion('4.3.5')
